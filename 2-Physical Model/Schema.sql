57e5398f14015f6b65bccd8e4936a14e301c9dcf
Alexandre Billard <alexandrebillard@Alexandres-MacBook-Pro.local>
2018-03-21 14:45:59 -0400

    Completed Schema

--- 2-Physical Model/Schema.sql
+++ 2-Physical Model/Schema.sql
@@ -22,10 +22,10 @@ CREATE TABLE location (
   canada boolean NOT NULL
 );
 
--- Product Dimension
+-- Disaster Dimension
 CREATE TABLE disaster (
   disasterKey int PRIMARY KEY,
-  disasterType text NOT NULL UNIQUE,
+  disasterType text NOT NULL,
   disasterSubgroup text NOT NULL,
   disasterGroup text NOT NULL,
   disasterCategory text NOT NULL,
@@ -33,12 +33,45 @@ CREATE TABLE disaster (
   utilityPeopleAffected int NOT NULL
 );
 
+-- Summary Dimension
+CREATE TABLE summary (
+  summaryKey int PRIMARY KEY,
+  summary text NOT NULL,
+  keyword1 text NOT NULL,
+  keyword2 text NOT NULL,
+  keyword3 text NOT NULL
+);
+
+-- costs Dimension
+CREATE TABLE costs (
+  costsKey int PRIMARY KEY,
+  estimatedTotalCost decimal,
+  normalizedTotalCost decimal,
+  federalPayments decimal,
+  provincialPayments decimal,
+  insurancePayments decimal
+);
+
+-- WeatherInfo Dimension
+CREATE TABLE populationStatistics (
+  popStatsKey int PRIMARY KEY,
+  description text NOT NULL
+);
+
+-- WeatherInfo Dimension
+CREATE TABLE weatherInfo (
+  weatherKey int PRIMARY KEY,
+  description text NOT NULL
+);
+
 -- Fact table
 CREATE TABLE factTable (
   startDateKey int NOT NULL REFERENCES date (dateKey),
   endDateKey int NOT NULL REFERENCES date (dateKey),
   locationKey int NOT NULL REFERENCES location (locationKey),
-  product int NOT NULL REFERENCES disaster (disasterkey)
+  disasterKey int NOT NULL REFERENCES disaster (disasterkey),
+  summaryKey int NOT NULL REFERENCES summary (summaryKey),
+  costKey int NOT NULL REFERENCES costs (costsKey),
+  popStatsKey int NOT NULL REFERENCES populationStatistics (popStatsKey),
+  weatherKey int NOT NULL REFERENCES weatherInfo (weatherKey)
 );
-
-
