eaf5f73c6aba76082d000106186ebc0c9b89b5e0
extraPaul <pllplnt2@gmail.com>
2018-04-04 23:58:24 -0400

    Made some changes to OLAP queries and graphs
    Still not done, just synching things up

--- 4-OLAP Queries/OLAPQueries.sql
+++ 4-OLAP Queries/OLAPQueries.sql
@@ -1,7 +1,6 @@
--- Total number of injuries in Canada in 1996 by province.
--- NOTE: not sure if I should just filter by start date? Or by end date?
+-- Total number of injuries and fatalities in Canada in 1996 by province.
 
-SELECT l.Province, sum(f.injured)
+SELECT l.Province, sum(f.injured), sum(f.fatalities)
 	FROM Location l, Fact_Table f, Date d1, Date d2
 	WHERE l.Canada = TRUE AND l.Province = 'ON' AND f.Location_Key = l.Location_Key
 	AND d1.Year = 1996 AND d2.Year = 1996 AND f.Start_Date_key = d1.Date_key AND f.End_Date_key = d2.Date_key
@@ -17,18 +16,18 @@ SELECT d1.Year, sum(f.fatalities) as deaths
 	LIMIT 10
 
 
--- Comparison of fatalities by disaster type in Ontario, in 2000
-SELECT l.Province,  d.Disaster_type, sum(f.fatalities)
+-- Comparison of fatalities by disaster type in British Columbia, in from the years 2000 to 2010
+SELECT d.Disaster_type, sum(f.fatalities)
 	FROM Location l, Fact_Table f, Disaster d, Date d1, Date d2
-	WHERE l.Canada = TRUE AND l.Province = 'ON' AND f.Location_Key = l.Location_Key AND f.Disaster_key = d.Disaster_key
-	AND d1.Year = 2000 AND d2.Year = 2000 AND f.Start_Date_key = d1.Date_key AND f.End_Date_key = d2.Date_key
+	WHERE l.Canada = TRUE AND l.Province = 'BC' AND f.Location_Key = l.Location_Key AND f.Disaster_key = d.Disaster_key
+	AND d1.Year >= 2000 AND d2.Year <= 2010 AND f.Start_Date_key = d1.Date_key AND f.End_Date_key = d2.Date_key
 	GROUP BY d.Disaster_type
 
 
 -- Cities in Canada with the most fires
-SELECT l.City,  d.Disaster_type, count(f) as num
+SELECT l.City, count(f) as num
 	FROM Location l, Fact_Table f, Disaster d
-	WHERE l.Canada = TRUE AND d.Disaster_Subgroup LIKE '%fire%'
+	WHERE l.Canada = TRUE AND d.Disaster_Subgroup LIKE '%Fire%'
 	AND f.Location_Key = l.Location_Key AND f.Disaster_key = d.Disaster_key
 	GROUP BY l.City
 	ORDER BY num DESC
@@ -43,7 +42,7 @@ SELECT d.disaster_subgroup, d.disaster_group, count(f)
 	FROM Disaster d INNER JOIN Fact_Table f ON d.Disaster_key = f.Disaster_key
 	GROUP BY d.disaster_subgroup
 
--- Or Drill down on average price of disasters per province, and per city
+-- Drill down on average price of disasters per province, and per city
 SELECT l.Province, avg(c.estimated_total_cost), avg(c.normalized_total_cost)
 	FROM Location l INNER JOIN Fact_Table f ON l.Location_key = f.Location_key
 	INNER JOIN Costs c ON f.Cost_key = c.Costs_key
@@ -53,4 +52,5 @@ SELECT l.City, avg(c.estimated_total_cost), avg(c.normalized_total_cost)
 	FROM Location l INNER JOIN Fact_Table f ON l.Location_key = f.Location_key
 	INNER JOIN Costs c ON f.Cost_key = c.Costs_key
 	GROUP BY l.City
+
 
